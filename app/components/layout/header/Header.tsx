f6d8f0bee451eea68dccc2e3292962c46a0f2df0
Elcho911 <boss.armsport@gmail.com>
2023-08-09 19:42:32 +0600

    WeDevX.co

--- app/components/layout/header/Header.tsx
+++ app/components/layout/header/Header.tsx
@@ -5,29 +5,27 @@ import Image from "next/image";
 import logo from "@/app/images/logo.png";
 
 const Header: FC = () => {
-	const links = [
-		{
-			href: "/",
-			label: "Home"
-		},
-		{
-			href: "/selenium",
-			label: "Elements"
-		}
-	];
+	// const links = [
+	// 	{
+	// 		href: "/selenium",
+	// 		label: "Get started"
+	// 	}
+	// ];
 
 	return (
 		<div className={scss.header}>
 			<div>
-				<Image className={scss.logo} priority={true} src={logo} alt="logo" />
+				<Link href="/">
+					<Image className={scss.logo} priority={true} src={logo} alt="logo" />
+				</Link>
 			</div>
-			<div>
+			{/* <div>
 				{links.map((items, index) => (
 					<Link key={index + 1} href={items.href}>
 						{items.label}
 					</Link>
 				))}
-			</div>
+			</div> */}
 		</div>
 	);
 };
--- app/components/pages/HomePage.tsx
+++ app/components/pages/HomePage.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from "react";
+import scss from "./Style.module.scss";
+import Link from "next/link";
+import { Button } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 const HomePage: FC = () => {
-	return <>HomePage</>;
+	return (
+		<>
+			<div className={scss.HomePage}>
+				<Link href="/selenium">
+					<Button variant="contained" size="large" endIcon={<SendIcon />}>
+						Get started
+					</Button>
+				</Link>
+			</div>
+		</>
+	);
 };
 
 export default HomePage;
