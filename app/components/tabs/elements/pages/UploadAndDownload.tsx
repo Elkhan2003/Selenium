1311952c445be75d279da7d2208cd747590b786f
Elcho911 <boss.armsport@gmail.com>
2023-08-10 02:50:25 +0600

    Temporarily fixed the photo download error

--- app/components/tabs/elements/pages/UploadAndDownload.tsx
+++ app/components/tabs/elements/pages/UploadAndDownload.tsx
@@ -1,22 +1,45 @@
 import React, { FC } from "react";
 import scss from "./Elements.module.scss";
+import axios from "axios";
 import { Button } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
 
 const UploadAndDownload: FC = () => {
+	const handleDownload = async () => {
+		try {
+			const response = await axios.get("https://i.ibb.co/Kw7F5BK/logo.png", {
+				responseType: "blob"
+			});
+
+			const blob = new Blob([response.data], { type: "image/png" });
+
+			const url = window.URL.createObjectURL(blob);
+
+			const link = document.createElement("a");
+			link.href = url;
+			link.setAttribute("download", "logo.png");
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+
+			window.URL.revokeObjectURL(url);
+		} catch (error) {
+			console.error("Error downloading file:", error);
+		}
+	};
+
 	return (
 		<>
 			<div className={scss.UploadAndDownload}>
-				<a
+				<Button
 					id="downloadButton"
-					href="https://i.ibb.co/Kw7F5BK/logo.png"
-					target="_blank"
-					download="logo.png"
+					variant="contained"
+					size="large"
+					onClick={handleDownload}
+					endIcon={<DownloadIcon />}
 				>
-					<Button variant="contained" size="large" endIcon={<DownloadIcon />}>
-						Download
-					</Button>
-				</a>
+					Download
+				</Button>
 				<form>
 					<label htmlFor="uploadFile">Select a file</label>
 					<input id="uploadFile" type="file" lang="en"></input>
