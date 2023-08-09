c9191bddc98d861422495db89ef6993ee6c5bf91
Elcho911 <boss.armsport@gmail.com>
2023-08-10 02:18:29 +0600

    Added Upload

--- app/components/tabs/elements/pages/UploadAndDownload.tsx
+++ app/components/tabs/elements/pages/UploadAndDownload.tsx
@@ -1,7 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import scss from "./Elements.module.scss";
+import { Button } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const UploadAndDownload: FC = () => {
-  return <>UploadAndDownload</>;
+	return (
+		<>
+			<div className={scss.UploadAndDownload}>
+				<a id="downloadButton" href="/public/logo.png" target="_blank" download>
+					<Button variant="contained" size="large" endIcon={<DownloadIcon />}>
+						Download
+					</Button>
+				</a>
+				<form>
+					<label htmlFor="uploadFile">Select a file</label>
+					<input id="uploadFile" type="file" lang="en"></input>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default UploadAndDownload;
