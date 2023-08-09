fde542491e946f91f46b8f23aacc160a2a96a05e
Elcho911 <boss.armsport@gmail.com>
2023-08-10 01:25:25 +0600

    Added Broken Link

--- app/components/tabs/elements/pages/BrokenLinksImages.tsx
+++ app/components/tabs/elements/pages/BrokenLinksImages.tsx
@@ -7,8 +7,24 @@ const BrokenLinksImages: FC = () => {
 	return (
 		<>
 			<div className={scss.BrokenLinksImages}>
-				<p>Valid image</p>
-				<Image src={picLogo} alt="pic-logo" />
+				<strong>Valid image</strong>
+				<Image className={scss.logo} src={picLogo} alt="logo" />
+				<br />
+				<strong>Broken image</strong>
+				<img src="/image/gg.png" alt="invalid-logo" />
+				<br />
+				<strong>Valid Link</strong>
+				<a href="https://www.wedevx.co/" target="_blank">
+					Click Here for Valid Link
+				</a>
+				<br />
+				<strong>Broken Link</strong>
+				<a
+					href="http://the-internet.herokuapp.com/status_codes/500"
+					target="_blank"
+				>
+					Click Here for Broken Link
+				</a>
 			</div>
 		</>
 	);
