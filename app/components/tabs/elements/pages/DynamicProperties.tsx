7f6473547f71700e191acf05d4cc426bce923b24
Elcho911 <boss.armsport@gmail.com>
2023-08-10 18:16:04 +0600

    Added DynamicProperties Buttons

--- app/components/tabs/elements/pages/DynamicProperties.tsx
+++ app/components/tabs/elements/pages/DynamicProperties.tsx
@@ -1,7 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import scss from "./Elements.module.scss";
+import { Button } from "@mui/material";
 
 const DynamicProperties: FC = () => {
-	return <>DynamicProperties</>;
+	const [randomId, setRandomId] = useState<string>("");
+
+	useEffect(() => {
+		const newRandomId = Math.random().toString(36).substr(2, 5);
+		setRandomId(newRandomId);
+	}, []);
+
+	return (
+		<>
+			<div className={scss.DynamicProperties}>
+				<p id={randomId}>This text has random Id</p>
+				<Button id="enableAfter" variant="contained" size="medium">
+					Will enable 5 seconds
+				</Button>
+				<Button id="colorChange" variant="contained" size="medium">
+					Color Change
+				</Button>
+				<Button id="visibleAfter" variant="contained" size="medium">
+					Visible After 5 Seconds
+				</Button>
+			</div>
+		</>
+	);
 };
 
 export default DynamicProperties;
