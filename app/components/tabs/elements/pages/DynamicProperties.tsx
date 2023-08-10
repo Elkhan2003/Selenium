f52fc716a15ec5730f6605c6afe495811079ba52
Elcho911 <boss.armsport@gmail.com>
2023-08-10 18:29:37 +0600

    Update DynamicProperties.tsx

--- app/components/tabs/elements/pages/DynamicProperties.tsx
+++ app/components/tabs/elements/pages/DynamicProperties.tsx
@@ -4,25 +4,44 @@ import { Button } from "@mui/material";
 
 const DynamicProperties: FC = () => {
 	const [randomId, setRandomId] = useState<string>("");
+	const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(false);
 
 	useEffect(() => {
 		const newRandomId = Math.random().toString(36).substr(2, 5);
 		setRandomId(newRandomId);
+
+		const enableTimeout = setTimeout(() => {
+			setIsButtonEnabled(true);
+		}, 5000);
+
+		return () => clearTimeout(enableTimeout);
 	}, []);
 
 	return (
 		<>
 			<div className={scss.DynamicProperties}>
 				<p id={randomId}>This text has random Id</p>
-				<Button id="enableAfter" variant="contained" size="medium">
+				<Button
+					id="enableAfter"
+					variant="contained"
+					size="medium"
+					disabled={!isButtonEnabled}
+				>
 					Will enable 5 seconds
 				</Button>
-				<Button id="colorChange" variant="contained" size="medium">
+				<Button
+					id="colorChange"
+					variant="contained"
+					size="medium"
+					style={isButtonEnabled ? { color: "red" } : {}}
+				>
 					Color Change
 				</Button>
-				<Button id="visibleAfter" variant="contained" size="medium">
-					Visible After 5 Seconds
-				</Button>
+				{isButtonEnabled && (
+					<Button id="visibleAfter" variant="contained" size="medium">
+						Visible After 5 Seconds
+					</Button>
+				)}
 			</div>
 		</>
 	);
