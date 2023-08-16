78f07a4d85f120b54b63ba158e841c77743071b7
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-08-16 15:36:49 +0600

    Added Frames

--- app/components/tabs/windows/pages/Frames.tsx
+++ app/components/tabs/windows/pages/Frames.tsx
@@ -4,8 +4,31 @@ import scss from "./Windows.module.scss";
 const Frames: FC = () => {
 	return (
 		<>
-			<div className={scss.Frames}>
-				<h1>Frames</h1>
+			<div id="framesWrapper" className={scss.Frames}>
+				<div className={scss.interval}>
+					Sample Iframe page There are 2 Iframes in this page. Use browser
+					inspecter or firebug to check out the HTML source. In total you can
+					switch between the parent frame, which is this window, and the two
+					frames below
+				</div>
+
+				<div id="frame1Wrapper" className={scss.interval}>
+					<iframe
+						src="/sample"
+						id="frame1"
+						width="500px"
+						height="350px"
+					></iframe>
+				</div>
+
+				<div id="frame2Wrapper" className={scss.interval}>
+					<iframe
+						src="/sample"
+						id="frame2"
+						width="100px"
+						height="100px"
+					></iframe>
+				</div>
 			</div>
 		</>
 	);
