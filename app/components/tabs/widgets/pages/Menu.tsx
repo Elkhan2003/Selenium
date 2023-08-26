0e04c2a60080484fb7d0e43e58bdd4ec0cf2bfa5
Elcho911 <boss.armsport@gmail.com>
2023-08-26 23:57:23 +0600

    Added Style for Menu

--- app/components/tabs/widgets/pages/Menu.tsx
+++ app/components/tabs/widgets/pages/Menu.tsx
@@ -1,27 +1,43 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
+import scss from './Widgets.module.scss'
 
 const Menu: FC = () => {
-
-  return <>Menu
-    <div className="nav-menu-container">
-      <ul id="nav">
-        <li><a href="#">Main Item 1</a></li>
-        <li><a href="#">Main Item 2</a>
-          <ul>
-            <li><a href="#">Sub Item</a></li>
-            <li><a href="#">Sub Item</a></li>
-            <li><a href="#">SUB SUB LIST »</a>
-              <ul>
-                <li><a href="#">Sub Sub Item 1</a></li>
-                <li><a href="#">Sub Sub Item 2</a></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li><a href="#">Main Item 3</a></li>
-      </ul>
-    </div>
-  </>;
+	return (
+		<>
+			<div className={scss.Menu}>
+				<ul id="nav" className={scss.nav}>
+					<li>
+						<a href="#">Main Item 1</a>
+					</li>
+					<li>
+						<a href="#">Main Item 2</a>
+						<ul>
+							<li>
+								<a href="#">Sub Item</a>
+							</li>
+							<li>
+								<a href="#">Sub Item</a>
+							</li>
+							<li>
+								<a href="#">SUB SUB LIST »</a>
+								<ul>
+									<li>
+										<a href="#">Sub Sub Item 1</a>
+									</li>
+									<li>
+										<a href="#">Sub Sub Item 2</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Main Item 3</a>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
 };
 
 export default Menu;
