ffe4bf28ddc4303419e1ebf6ffd5b96956860214
Elcho911 <boss.armsport@gmail.com>
2023-10-24 00:46:53 +0600

    minor update

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -4,7 +4,12 @@ import scss from "./Footer.module.scss";
 const Footer: FC = () => {
 	return (
 		<div className={scss.footer}>
-			<p>© 2018-2023 WEDEVX.CO | ALL RIGHTS RESERVED | By Elcho911</p>
+			<p>
+				© 2018-2023 WEDEVX.CO | ALL RIGHTS RESERVED |&nbsp;
+				<a href="https://www.instagram.com/elcho911/" target="_blank">
+					By Elcho911
+				</a>
+			</p>
 		</div>
 	);
 };
